9953c77edfc3ccf2427c4c86ccfd579006882242
ftb2024_official <ftb2024@gmail.com>
2024-07-15 11:20:42 +0500

    one example slightly updated

--- interfaces/example-1.go
+++ interfaces/example-1.go
@@ -13,26 +13,49 @@ type Circle struct {
 	Radius float64
 }
 
+type Rectangle struct {
+	Length, Width float64
+}
+
 type Square struct {
 	Side float64
 }
 
-func (c *Circle) Area() float64 {
+func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
-func (s *Square) Area() float64 {
+func (r Rectangle) Area() float64 {
+	return r.Length * r.Width
+}
+
+func (s Square) Area() float64 {
 	return s.Side * s.Side
 }
 
 func printArea(sh Shape) {
-	fmt.Println("Area:", sh.Area())
+	fmt.Printf("%.2f\n", sh.Area())
+}
+
+func totalArea(shapes []Shape) float64 {
+	var total float64
+
+	for _, sh := range shapes {
+		total += sh.Area()
+	}
+
+	return total
 }
 
 func Example1() {
-	circle := Circle{13}
-	square := Square{21}
+	c := Circle{10}
+	r := Rectangle{20, 30}
+	s := Square{40}
 
-	printArea(&circle)
-	printArea(&square)
+	shapes := []Shape{c, r, s}
+
+	printArea(c)
+	printArea(r)
+	printArea(s)
+	fmt.Printf("Total area: %.2f\n", totalArea(shapes))
 }
