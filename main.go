45613d122ca6273dea94166fba62869a6f1f5713
ftb2024_official <ftb2024@gmail.com>
2024-11-06 21:52:05 +0500

    one pointer example and pattern folder created

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import test "go-basics/channels-and-goroutines"
+import "fmt"
+
+func f1(n int) float64 {
+	result := 10_000.
+	for i := 0; i < n; i++ {
+		result += 1.0001
+	}
+	return result
+}
+
+func f2(n int) float64 {
+	result := 0.
+	for i := 0; i < n; i++ {
+		result += 1.0001
+	}
+	return result + 10_000.
+}
 
 func main() {
-	test.Example6()
+	v1 := f1(1000)
+	v2 := f2(1000)
+
+	fmt.Println(v1)
+	fmt.Println(v2)
 }
