3df75fbb21681512f8857dc4fbd8d1910cae9e2c
ftb2024_official <ftb2024@gmail.com>
2025-01-30 11:05:53 +0500

    test for pattern factory method

--- patterns/CREATIONAL/FactoryMethod/factory_method_test.go
+++ patterns/CREATIONAL/FactoryMethod/factory_method_test.go
@@ -1,1 +1,19 @@
 package factory_method
+
+import "testing"
+
+func TestFactoryMethod(t *testing.T) {
+	assert := []string{"A", "B", "C"}
+	factory := NewCreator()
+	products := []Product{
+		factory.CreateProduct(A),
+		factory.CreateProduct(B),
+		factory.CreateProduct(C),
+	}
+
+	for i, product := range products {
+		if action := product.Use(); action != assert[i] {
+			t.Errorf("want action %v, got action %v", assert[i], action)
+		}
+	}
+}
